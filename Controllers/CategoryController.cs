fff6b672875688916dcc5d9c68337437a410ea20
EdsonSantosCode <dev.escode@hotmail.com>
2022-11-11 06:34:11 -0300

    Tratando possíveis erros

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -30,10 +30,21 @@ public class CategoryController : ControllerBase
     [HttpPost("v1/categories")]
     public async Task<IActionResult> PostAsync([FromBody] Category model, [FromServices] BlogDataContext context)
     {
-        await context.Categories.AddAsync(model);
-        await context.SaveChangesAsync();
+        try
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
 
-        return Created($"v1/categories/{model.Id}", model);
+            return Created($"v1/categories/{model.Id}", model);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "05XE9 - Não foi possível incluir a categoria.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05X10 - Um erro inesperado ocorreu!");
+        }
     }
 
     [HttpPut("v1/categories/{id:int}")]
@@ -41,33 +52,57 @@ public class CategoryController : ControllerBase
                                                     [FromBody] Category model,
                                                     [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories
-            .FirstOrDefaultAsync(x => x.Id == id);
+        try
+        {
+            var category = await context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        if (category is null)
-            return NotFound();
+            if (category is null)
+                return NotFound();
 
-        category.Name = model.Name;
-        category.Slug = model.Slug;
+            category.Name = model.Name;
+            category.Slug = model.Slug;
 
-        context.Categories.Update(category);
-        await context.SaveChangesAsync();
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "05XE8 - Não foi possível incluir a categoria.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05X11 - Um erro inesperado ocorreu!");
+        }
+
+
     }
 
     [HttpDelete("v1/categories/{id:int}")]
     public async Task<IActionResult> DeleteAsync(int id, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories
-            .FirstOrDefaultAsync(x => x.Id == id);
+        try
+        {
+            var category = await context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        if (category is null)
-            return NotFound();
+            if (category is null)
+                return NotFound();
 
-        context.Categories.Remove(category);
-        await context.SaveChangesAsync();
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "05XE7 - Não foi possível incluir a categoria.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05X12 - Um erro inesperado ocorreu!");
+        }
     }
 }
