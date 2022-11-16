8e1834379e958b409cfcddaeadf0e6d66d2ec95e
EdsonSantosCode <dev.escode@hotmail.com>
2022-11-16 06:03:14 -0300

    Implementado ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@ namespace Blog;
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "MTBjNDgzODUtNzk5Yy00ZWE4LWE2MzUtNzZjOTgxMDZkM2Jl";
+    public static string JwtKey = "MTBjNDgzODUtNzk5Yy00ZWE4LWE2MzUtNzZjOTgxMDZkM2Jl";
+    public static string ApiKeyName =  "api_key";
+    public static string ApiKey = "MmIwNGVlZTAtMjJhYS00YjlkLWFkZjktZDc0MTc1ZDlkYjky";
 }
