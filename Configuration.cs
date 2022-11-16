de0ca054bd362ffde2d85668b75146f7960aaa16
EdsonSantosCode <dev.escode@hotmail.com>
2022-11-16 07:02:49 -0300

    retirando hard code

--- Configuration.cs
+++ Configuration.cs
@@ -2,7 +2,16 @@ namespace Blog;
 
 public static class Configuration
 {
-    public static string JwtKey = "MTBjNDgzODUtNzk5Yy00ZWE4LWE2MzUtNzZjOTgxMDZkM2Jl";
-    public static string ApiKeyName =  "api_key";
-    public static string ApiKey = "MmIwNGVlZTAtMjJhYS00YjlkLWFkZjktZDc0MTc1ZDlkYjky";
+    public static string JwtKey { get; set; }
+    public static string ApiKeyName { get; set; }
+    public static string ApiKey { get; set; }
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
